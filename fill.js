08382b0ba5d2309823359c28f239f738cd411afa
Vladimir Gorunov <vgorunov@appulate.com>
2020-09-23 06:00:03 +0300

    Test site scripts added

---
+++ fill.js
@@ -0,0 +1,8 @@
+$(window).load(() => {
+    // You need to fill all forms by corresponding fields from global "data" object 
+    console.log(data);
+
+    $("#btnFill").click(() => {
+        alert("No filling introduced yet");
+    });
+})
